fc98857a5c2f5813c494e142a0bbf24aae46a2ec
DoanHieu <minhhieu.doan1802@gmail.com>
2025-01-16 22:36:43 +0700

    add sent mail function

--- back_end/server.js
+++ back_end/server.js
@@ -1,10 +1,20 @@
 const express = require('express')
 const PORT = 3000
-
 const app = express()
 
-app.get('/', (req, res))
+// IMPORT MODULE 
+
+const sentMail = require('./myModule/mail.js')
+
+// ----------------------------------------------------------
+
+// CREATE API
+
+app.use('/', sentMail);
+
+// ----------------------------------------------------------
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost: ${PORT}`)
+    console.log(`Server is running on http://localhost:${PORT}`)
+    fetch('http://localhost:3000/send_code')
 })
