55e2fd6867066436389678fc6ae09759174fd6e3
hhh102 <hhhttct102@gmail.com>
2025-02-09 21:07:44 +0700

    Sua lai header

--- front_end/Login/login.js
+++ front_end/Login/login.js
@@ -1,46 +1,43 @@
 //client
 
-document.getElementById('loginButton').addEventListener('click', login);
-
+document.getElementById("loginButton").addEventListener("click", login);
 
 async function login() {
-    // get information form login form
-    const account = document.getElementById('username').value; // mail or userName
-    const password = document.getElementById('password').value;
-    console.log('acc:', account)
-    console.log('pass:', password)
+  // get information form login form
+  const account = document.getElementById("username").value; // mail or userName
+  const password = document.getElementById("password").value;
+  console.log("acc:", account);
+  console.log("pass:", password);
 
-    if (!account || !password) {
-        alert("Invalid account or password!");
-        return;
-    }
+  if (!account || !password) {
+    alert("Invalid account or password!");
+    return;
+  }
 
-    try {
-        // Send login request to server
-        await fetch("http://localhost:3000/checkAccountExist", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ account, password })
-        })
-            .then(response => { return response.json() })
-            .then(result => {
-
-                if (result.id === -1) {
-                    alert("Invalid account or password")
-                } else {
-                    localStorage.setItem('loggedIn', 'true');
-                    localStorage.setItem('userID', result.id);
-                    localStorage.setItem('role', result.role);
-                    window.location.href = "../HomePage/HomePage.html";
-                }
-            })
-
-
-    } catch (error) {
-
-        console.error("Error for login requestrequest:", error);
-        alert("Error, try again.");
-    }
+  try {
+    // Send login request to server
+    await fetch("http://localhost:3000/checkAccountExist", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ account, password }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((result) => {
+        if (result.id === -1) {
+          alert("Invalid account or password");
+        } else {
+          localStorage.setItem("loggedIn", "true");
+          localStorage.setItem("userID", result.id);
+          localStorage.setItem("role", result.role);
+          window.location.href = "../HomePage/HomePage.html";
+        }
+      });
+  } catch (error) {
+    console.error("Error for login requestrequest:", error);
+    alert("Error, try again.");
+  }
 }
