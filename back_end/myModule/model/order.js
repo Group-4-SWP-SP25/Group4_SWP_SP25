c00dc33301e8f161f8c4182284866e08d63ec609
hhh102 <hhhttct102@gmail.com>
2025-02-14 16:23:59 +0700

    Tu dong them car part khi insert car

---
+++ back_end/myModule/model/order.js
@@ -0,0 +1,30 @@
+class Order {
+  constructor(orderData) {
+    this.userID = userData.UserID;
+    this.orderID = userData.OrderID;
+    this.password = userData.Password;
+    this.firstName = userData.FirstName;
+    this.lastName = userData.LastName;
+    this.email = userData.Email;
+    this.address = userData.Address;
+    this.role = userData.Role;
+    this.phone = userData.Phone;
+  }
+  userInfo() {
+    let data = {
+      userID: this.userID,
+      userName: this.userName,
+      password: this.password,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      email: this.email,
+      address: this.address,
+      role: this.role,
+      phone: this.phone,
+    };
+    return data;
+  }
+}
+
+module.exports = Order;
+``;
