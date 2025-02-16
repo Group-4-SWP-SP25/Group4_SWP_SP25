3e5205a4b3615ee41caf34b9d39877ca6902b894
HoangNLHE180286 <hoangnlhe180286@fpt.edu.vn>
2025-02-17 01:07:02 +0700

    Update ServiceDetail

--- back_end/myModule/database/user/getServiceTypeDetail.js
+++ back_end/myModule/database/user/getServiceTypeDetail.js
@@ -1,36 +1,31 @@
-const sql = require("mssql");
+const sql = require('mssql');
 const getServiceDetailByName = async (req, res) => {
-  try {
-    console.log(req.body);
-    const { serviceTypeName } = req.body;
-    console.log(serviceTypeName);
+    try {
+        // console.log(req.body);
+        const { serviceTypeName } = req.body;
+        // console.log(serviceTypeName);
 
-    if (!serviceTypeName) {
-      return res.status(400).json({ error: "Service type name is required" });
-    }
+        if (!serviceTypeName) {
+            return res.status(400).json({ error: 'Service type name is required' });
+        }
 
-    const pool = global.pool;
+        const pool = global.pool;
 
-    if (!pool) {
-      return res
-        .status(500)
-        .json({ error: "Database connection not initialized" });
-    }
+        if (!pool) {
+            return res.status(500).json({ error: 'Database connection not initialized' });
+        }
 
-    const query = `
-            SELECT s.*
-            FROM Services s
-            JOIN ServiceTypes st ON s.ServiceTypeID = st.ServiceTypeID
+        const query = `
+            SELECT st.ServiceTypeName, s.* 
+            FROM [Service] s 
+            JOIN ServiceTypes st ON s.ServiceTypeID = st.ServiceTypeID 
             WHERE st.ServiceTypeName = @serviceTypeName`;
-    const result = await pool
-      .request()
-      .input("serviceTypeName", sql.VarChar, serviceTypeName)
-      .query(query);
-    console.log(result.recordset);
-    res.json(result.recordset);
-  } catch (err) {
-    console.log(err);
-  }
+        const result = await pool.request().input('serviceTypeName', sql.VarChar, serviceTypeName).query(query);
+        // console.log(result.recordset);
+        res.json(result.recordset);
+    } catch (err) {
+        console.log(err);
+    }
 };
 
 module.exports = getServiceDetailByName;
