f527b32e5a1fb7d7a281327b3630a3eb84b826d2
HoangNLHE180286 <hoangnlhe180286@fpt.edu.vn>
2025-02-18 02:57:18 +0700

    Update ServiceDetail

--- front_end/ServiceDetail/script.js
+++ front_end/ServiceDetail/script.js
@@ -1,7 +1,8 @@
 const urlParams = new URLSearchParams(window.location.search);
 const service = urlParams.get('serviceTypeName');
 
-const descDiv = document.querySelector(".desc");
+const infoDiv = document.querySelector('.info');
+const detailDiv = document.querySelector('.detail');
 
 async function getServiceTypeDetail() {
     const response = await fetch('http://localhost:3000/getServiceTypeDetail', {
@@ -10,10 +11,22 @@ async function getServiceTypeDetail() {
         body: JSON.stringify({
             serviceTypeName: service
         })
-    })
+    });
     const result = await response.json();
-    document.querySelector('.desc-name').innerHTML = result[0].ServiceName;
+    console.log(typeof result);
     console.log(result);
+
+    document.querySelector('.info-name').innerHTML = result[0].ServiceTypeName;
+    document.querySelector('.info-desc').innerHTML = result[0].ServiceTypeDescription;
+
+    for (const key in result) {
+        const item = result[key];
+
+        const detailItem = document.createElement('p');
+        detailItem.classList.add('detail-item');
+        detailItem.textContent = item.ServiceName + ': ' + item.ServiceDescription + ' - ' + item.ServicePrice + ' VND';
+
+        detailDiv.appendChild(detailItem);
+    }
 }
 getServiceTypeDetail();
-
